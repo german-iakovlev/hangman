ca81a479c6d30a4598235a6bc07d49c8a3696906
german-iakovlev <g.iakovlev@gmail.com>
2018-04-20 17:02:09 +0300

    Add pixel tracking for testing purposes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
+import base64
 
 from resources.game import game_api
 
@@ -15,6 +16,16 @@ def hangman():
 def index():
     return render_template('index.html')
 
+@app.route('/fb')
+def fb():
+    
+    if request.cookies.get('cookie_fb'):
+        return "You have a cookie"
+    else:
+        gif = 'R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+        res = make_response(base64.b64decode(gif))
+        res.set_cookie('cookie_fb', '12345', max_age=60 * 60 * 24 * 365 * 2)
+        return res
 
 @app.errorhandler(404)
 def page_not_found(e):
